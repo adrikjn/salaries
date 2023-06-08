ba4cc8a71d2812df8cb3f79b3e9fbccdc9c6f512
Adrien KOUYOUMJIAN <adrien.kouyoumjian@outlook.fr>
2023-06-08 13:16:45 +0200

    ajout et showall

--- src/Controller/EmployesController.php
+++ src/Controller/EmployesController.php
@@ -2,17 +2,44 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Employes;
+use App\Form\EmployeType;
+use App\Repository\EmployesRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EmployesController extends AbstractController
 {
     #[Route('/', name: 'employes')]
-    public function index(): Response
-    {
+    public function index(EmployesRepository $repo): Response
+    {   
+        $employes = $repo->findAll();
         return $this->render('employes/index.html.twig', [
-            'controller_name' => 'EmployesController',
+            "employes" => $employes
+        ]);
+    }
+
+    #[Route('/ajout/employes', name: 'form')]
+    public function form(Request $globals, EntityManagerInterface $manager){
+        
+        $employes = new Employes;
+
+        $formEmploye = $this->createForm(EmployeType::class, $employes);
+
+        $formEmploye->handleRequest($globals);
+
+        if($formEmploye->isSubmitted() && $formEmploye->isValid()) {
+            $manager->persist($employes);
+            $manager->flush();
+            return $this->redirectToRoute("employes");
+        }
+
+        return $this->renderForm('employes/form.html.twig', [
+            'formEmploye' => $formEmploye,
         ]);
     }
 }
+ 
